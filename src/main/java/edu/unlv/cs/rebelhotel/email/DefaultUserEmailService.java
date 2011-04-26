cdddbb96bbf66ee7f0861e5ee198f645f2b7ad4e
Greg <grcarter@unlv.nevada.edu>
2011-04-25 19:47:35 -0700

    Async queue added

--- src/main/java/edu/unlv/cs/rebelhotel/email/DefaultUserEmailService.java
+++ src/main/java/edu/unlv/cs/rebelhotel/email/DefaultUserEmailService.java
@@ -2,6 +2,8 @@ package edu.unlv.cs.rebelhotel.email;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 import javax.mail.internet.MimeMessage;
 
@@ -18,6 +20,7 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.velocity.VelocityEngineFactoryBean;
 import org.springframework.ui.velocity.VelocityEngineUtils;
@@ -30,7 +33,9 @@ import edu.unlv.cs.rebelhotel.domain.WorkEffort;
 public class DefaultUserEmailService implements UserEmailService{
 
 	private static final Logger LOG = LoggerFactory.getLogger(DefaultUserEmailService.class);
+	private static final int MAX_BATCH = 500;
 	private JavaMailSender mailSender;
+	private Queue<MimeMessagePreparator> mailQueue = new ConcurrentLinkedQueue<MimeMessagePreparator>();
 
 
 	public JavaMailSender getMailSender() {
@@ -48,32 +53,40 @@ public class DefaultUserEmailService implements UserEmailService{
 	}
 
 	private void send(MimeMessagePreparator preparator){
+		
 		try{
 			this.mailSender.send(preparator);
 		} 
         catch (MailAuthenticationException ex) {
-            //log it and go on
             LOG.warn(" Mail Server Failed Authorization", ex);            
         }
         catch(MailParseException ex){
-        	//log it and go on
         	LOG.warn(" Unable To Parse Email Message", ex);
         }
         catch(MailPreparationException ex){
-        	//log it and go on
-        	LOG.warn(" Mail Preparation Failure ", ex);
+        	LOG.error(" Mail Preparation Failure ", ex);
         }
         catch(MailSendException ex){
-        	//log it and go on
         	LOG.warn(" Error Sending Mail Send Exception ", ex);
-        }     
+        }
+	}
+	
+	private void enqueue(MimeMessagePreparator preparator){
+		mailQueue.add(preparator);
+		
     }
+	@Scheduled(fixedDelay=60000)
+	public void processBatch(){
+		for(int i =0; i< MAX_BATCH && !mailQueue.isEmpty(); i++){
+			send(mailQueue.remove());
+		}
+			
+	}
 	
-	
-    @Async    
+      
 	public void sendStudentConfirmation(final UserAccount userAccount, final String password)  {
 
-		send(new MimeMessagePreparator(){
+		enqueue(new MimeMessagePreparator(){
 
 			public void prepare(MimeMessage mimeMessage) throws Exception {
 				MimeMessageHelper message = new MimeMessageHelper(mimeMessage);
@@ -95,10 +108,10 @@ public class DefaultUserEmailService implements UserEmailService{
 	}
 	
 	
-	@Async
+	
 	public void sendAdminComfirmation(final UserAccount userAccount, final String password)  {
 		
-		send(new MimeMessagePreparator() {
+		enqueue(new MimeMessagePreparator() {
 			public void prepare(MimeMessage mimeMessage) throws Exception {
 				MimeMessageHelper message = new MimeMessageHelper(mimeMessage);
 				message.setTo(userAccount.getEmail());
@@ -119,10 +132,10 @@ public class DefaultUserEmailService implements UserEmailService{
 }
 	
 	
-	@Async
+	
 	public void sendNewPassword(final UserAccount userAccount, final String password) {
 		
-		send(new MimeMessagePreparator() {
+		enqueue(new MimeMessagePreparator() {
 			public void prepare(MimeMessage mimeMessage) throws Exception {
 				MimeMessageHelper message = new MimeMessageHelper(mimeMessage);
 				message.setTo(userAccount.getEmail());
@@ -142,10 +155,10 @@ public class DefaultUserEmailService implements UserEmailService{
 		
 }
 	
-	@Async
+	
 	public void sendWorkConfirmation(final Student student, final WorkEffort workEffort){
 		
-		send(new MimeMessagePreparator() {
+		enqueue(new MimeMessagePreparator() {
 			public void prepare(MimeMessage mimeMessage) throws Exception {
 				MimeMessageHelper message = new MimeMessageHelper(mimeMessage);
 				message.setTo(student.getUserAccount().getEmail());
